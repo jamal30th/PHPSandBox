ba0a9de1ad7743ebd4a9dac530775ad61fa9dea3
Muhammad Jamal Uddin <jamal30th@gmail.com>
2019-05-08 02:57:14 +0600

    php front to back part 1

---
+++ variables.php
@@ -0,0 +1,13 @@
+//part 1:
+    <?php
+    $num1 = 4;
+    $num2 = 6;
+    $sum = $num1 + $num2;
+    // echo $sum;
+    $string1 = 'Hello';
+    $string2 = 'World';
+    $greeting = $string1.' '.$string2.'!';
+$greeting1 = "$string1 $string2";
+define('greetings', 'Hello everyone', true);
+echo greetings;
+    ?>
